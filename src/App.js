8e536bb0f0ef7c5e26f340c30115f7f8233b29cd
Mandar Waghe <mandar.waghe@udgama.com>
2020-12-10 21:19:07 +0530

    Completed sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 app__sidebar">
+            <Sidebar />
+          </div>
+          <div className="col-lg-9 app__main-content">
+            <Main />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { FiInstagram } from "react-icons/fi";
+import { AiFillPushpin } from "react-icons/ai";
+// import { FaAward } from "react-icons/fa";
+import { AiOutlineDownload } from "react-icons/ai";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <img
+        src="https://mandarwaghe.netlify.app/static/profile-f5777bd0aeb0b223c161fe760357720b.jpeg"
+        alt="profile"
+        className="sidebar__avatar"
+      />
+      <div className="sidebar__name">
+        Mandar <span>Waghe</span>
+      </div>
+      <div className="sidebar__item sidebar_title">Software Engineer</div>
+      <a href="" className="link">
+        <div className="sidebar__item">
+          <AiOutlineDownload className="sidebar__icon" /> Download Resume
+        </div>
+      </a>
+      <figure className="sidebar__social-icons my-2">
+        <a href="">
+          <FaFacebookF className="sidebar__icon mr-3" />
+        </a>
+        <a href="">
+          <FiInstagram className="sidebar__icon mr-3" />
+        </a>
+        <a href="">
+          <AiFillGithub className="sidebar__icon mr-3" />
+        </a>
+      </figure>
+      <div className="sidebar__contact">
+        <div className="sidebar__location">
+          <AiFillPushpin className="sidebar__icon" />
+          Maharashtra, India
+        </div>
+        <div className="sidebar__item">mandar@gmail.com</div>
+        <div className="sidebar_item">8549489943</div>
+      </div>
+      <div className="sidebar__email">Email Me</div>
+    </div>
+  );
+};
+
+export default Sidebar;
