2c26537d635fdd4695e9a63936c1cf34cf054b43
Mandar Waghe <mandar.waghe@udgama.com>
2020-12-10 22:14:31 +0530

    Updated navbar to routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
-import Main from "./components/Main";
+// import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import { Route, Redirect, Switch } from "react-router-dom";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -13,6 +17,20 @@ function App() {
           </div>
           <div className="col-lg-9 app__main-content">
             <Navbar />
+            <Switch>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/resume">
+                <Resume />
+              </Route>
+              <Route exact path="/projects">
+                <Projects />
+              </Route>
+              <Route>
+                <Redirect to="/about" />
+              </Route>
+            </Switch>
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [active, setActive] = useState("About");
+  const [active, setActive] = useState("");
+  useEffect(() => {
+    let currentUrl = window.location.href;
+    console.log(currentUrl);
+    if (currentUrl.endsWith("/about")) setActive("About");
+    else if (currentUrl.endsWith("/resume")) setActive("Resume");
+    else if (currentUrl.endsWith("/projects")) setActive("Projects");
+  }, [active]);
   return (
     <div className="navbar">
       <div className="navbar__active">{active}</div>
       <div className="navbar__items">
         {active !== "About" && (
-          <div className="navbar__item" onClick={() => setActive("About")}>
-            About
-          </div>
+          <Link to="/about" className="navbar__link">
+            <div className="navbar__item" onClick={() => setActive("About")}>
+              About
+            </div>
+          </Link>
         )}
 
         {active !== "Resume" && (
-          <div className="navbar__item" onClick={() => setActive("Resume")}>
-            Resume
-          </div>
+          <Link to="/resume" className="navbar__link">
+            <div className="navbar__item" onClick={() => setActive("Resume")}>
+              Resume
+            </div>
+          </Link>
         )}
         {active !== "Projects" && (
-          <div className="navbar__item" onClick={() => setActive("Projects")}>
-            Projects
-          </div>
+          <Link to="/projects" className="navbar__link">
+            <div className="navbar__item" onClick={() => setActive("Projects")}>
+              Projects
+            </div>
+          </Link>
         )}
       </div>
     </div>
